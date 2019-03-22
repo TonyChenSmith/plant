abbe029616c11884f281b28a0a56a0dc0de15de8
TonyChenSmith <a2898113823@163.com>
2019-03-22 22:32:48 +0800

    background update

---
+++ Plant/plant/src/main/java/org/tonygatins/tonysmith/xjikll/plant/context/drawable/BackgroundDrawable.java
@@ -0,0 +1,62 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Tony Chen Smith and xjikll
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.tonygatins.tonysmith.xjikll.plant.context.drawable;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+
+/**
+ * 背景用的图片，在这里做出一定的修改。
+ * @author TonyChenSmith
+ */
+public final class BackgroundDrawable extends Drawable
+{
+
+	@Override
+	public void draw(Canvas p1)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setAlpha(int p1)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setColorFilter(ColorFilter p1)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public int getOpacity()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+	
+}
