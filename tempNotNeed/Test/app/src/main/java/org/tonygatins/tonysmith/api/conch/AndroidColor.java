edb5ca01bef2e28be1146e6da35ecc058dda553b
TonyChenSmith <a2898113823@163.com>
2019-06-17 03:23:04 +0800

    颜料类实现

--- tempNotNeed/Test/app/src/main/java/org/tonygatins/tonysmith/api/conch/AndroidColor.java
+++ tempNotNeed/Test/app/src/main/java/org/tonygatins/tonysmith/api/conch/AndroidColor.java
@@ -2,6 +2,138 @@ package org.tonygatins.tonysmith.api.conch;
 
 import org.tonygatins.tonysmith.api.conch.resource.Color;
 
-public class AndroidColor extends Color
+/**
+ * 安卓底层实现：颜料。
+ * @author Tony Chen Smith
+ */
+final class AndroidColor extends Color
 {
+	
+	
+	@Override
+	public int getNativeColor()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+
+	@Override
+	public int getAlpha()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+
+	@Override
+	public int getRed()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+
+	@Override
+	public int getGreen()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+
+	@Override
+	public int getBlue()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+
+	@Override
+	public float[] getHSV()
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	protected Color nativeCreateColor(int alpha,float[] hsv)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	protected Color nativeCreateColor(float[] hsv)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	protected Color nativeCreateColor(String colorText)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	protected Color nativeCreateColor(int red,int green,int blue)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	protected Color nativeCreateColor(int alpha,int red,int green,int blue)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	public void setColor(int alpha,float[] hsv)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setColor(float[] hsv)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setColor(String colorText)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setColor(int red,int green,int blue)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setColor(int alpha,int red,int green,int blue)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setColor(Color parent)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void recycle()
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public boolean isRecycled()
+	{
+		// TODO: Implement this method
+		return false;
+	}
+	
 }
--- tempNotNeed/Test/app/src/main/java/org/tonygatins/tonysmith/api/conch/resource/Color.java
+++ tempNotNeed/Test/app/src/main/java/org/tonygatins/tonysmith/api/conch/resource/Color.java
@@ -69,46 +69,129 @@ public abstract class Color
 	 */
 	protected abstract Color nativeCreateColor(String colorText);
 	
+	/**
+	 * 本地通过红色值，绿色值，蓝色值构造颜料对象。
+	 * @param red 红色值。
+	 * @param green 绿色值。
+	 * @param blue 蓝色值。
+	 * @return 一个相对应的颜料对象。
+	 */
 	protected abstract Color nativeCreateColor(int red,int green,int blue);
 	
+	/**
+	 * 本地通过透明度值，红色值，绿色值，蓝色值构造颜料对象。
+	 * @param alpha 透明度值。
+	 * @param red 红色值。
+	 * @param green 绿色值。
+	 * @param blue 蓝色值。
+	 * @return 一个相对应的颜料对象。
+	 */
 	protected abstract Color nativeCreateColor(int alpha,int red,int green,int blue);
 	
+	/**
+	 * 通过透明度值与直观颜色数组对象设置颜料对象。
+	 * @param alpha 透明度整型值。
+	 * @param hsv 直观颜色数组对象。
+	 */
 	public abstract void setColor(int alpha,float[] hsv);
 	
+	/**
+	 * 通过直观颜色数组对象设置颜料对象。
+	 * @param hsv 直观颜色数组对象。
+	 */
 	public abstract void setColor(float[] hsv);
 	
+	/**
+	 * 通过颜色字符串对象设置颜料对象。
+	 * @param colorText 颜色字符串对象。
+	 */
 	public abstract void setColor(String colorText);
 	
+	/**
+	 * 通过红色值，绿色值，蓝色值设置颜料对象。
+	 * @param red 红色值。
+	 * @param green 绿色值。
+	 * @param blue 蓝色值。
+	 */
 	public abstract void setColor(int red,int green,int blue);
 	
+	/**
+	 * 通过透明度值，红色值，绿色值，蓝色值设置颜料对象。
+	 * @param alpha 透明度值。
+	 * @param red 红色值。
+	 * @param green 绿色值。
+	 * @param blue 蓝色值。
+	 */
 	public abstract void setColor(int alpha,int red,int green,int blue);
 	
+	/**
+	 * 通过颜料对象重新设置颜料对象。
+	 * @param parent 父辈颜料对象。
+	 */
 	public abstract void setColor(Color parent);
 	
+	/**
+	 * 回收方法。（不一定会进行有效实现）
+	 */
 	public abstract void recycle();
 	
+	/**
+	 * 判断是否已回收。
+	 * @return true为已回收，false为未回收。
+	 */
 	public abstract boolean isRecycled();
 	
+	/**
+	 * 通过透明度值与直观颜色数组对象构造颜料对象。
+	 * @param alpha 透明度整型值。
+	 * @param hsv 直观颜色数组对象。
+	 * @return 一个相对应的颜料对象。
+	 */
 	public final static Color createColor(int alpha,float[] hsv)
 	{
 		return NATIVE_HANDLER.nativeCreateColor(alpha,hsv);
 	}
 	
+	/**
+	 * 本地通过直观颜色数组对象构造颜料对象。
+	 * @param hsv 直观颜色数组对象。
+	 * @return 一个相对应的颜料对象。
+	 */
 	public final static Color createColor(float[] hsv)
 	{
 		return NATIVE_HANDLER.nativeCreateColor(hsv);
 	}
 	
+	/**
+	 * 本地通过颜色字符串对象构造颜料对象。
+	 * @param colorText 颜色字符串对象。
+	 * @return 一个相对应的颜料对象。
+	 */
 	public final static Color createColor(String colorText)
 	{
 		return NATIVE_HANDLER.nativeCreateColor(colorText);
 	}
 	
+	/**
+	 * 本地通过红色值，绿色值，蓝色值构造颜料对象。
+	 * @param red 红色值。
+	 * @param green 绿色值。
+	 * @param blue 蓝色值。
+	 * @return 一个相对应的颜料对象。
+	 */
 	public final static Color createColor(int red,int green,int blue)
 	{
 		return NATIVE_HANDLER.nativeCreateColor(red,green,blue);
 	}
 	
+	/**
+	 * 本地通过透明度值，红色值，绿色值，蓝色值构造颜料对象。
+	 * @param alpha 透明度值。
+	 * @param red 红色值。
+	 * @param green 绿色值。
+	 * @param blue 蓝色值。
+	 * @return 一个相对应的颜料对象。
+	 */
 	public final static Color createColor(int alpha,int red,int green,int blue)
 	{
 		return NATIVE_HANDLER.nativeCreateColor(alpha,red,green,blue);
