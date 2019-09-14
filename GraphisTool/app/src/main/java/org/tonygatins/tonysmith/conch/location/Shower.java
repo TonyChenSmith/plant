88da9938041a0aac8742148f270e4c016e0e9c6e
TonyChenSmith <a2898113823@163.com>
2019-09-14 17:29:58 +0800

    字体别名分析

--- GraphisTool/app/src/main/java/org/tonygatins/tonysmith/conch/location/Shower.java
+++ GraphisTool/app/src/main/java/org/tonygatins/tonysmith/conch/location/Shower.java
@@ -1,9 +1,14 @@
 package org.tonygatins.tonysmith.conch.location;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import org.tonygatins.tonysmith.conch.R;
 import android.view.KeyEvent;
+import android.widget.Toast;
 
 /**
  * 主界面。显示引擎画面。
@@ -11,12 +16,25 @@ import android.view.KeyEvent;
  */
 public class Shower extends AppCompatActivity
 {
+	//读取请求码
+	private static final int REQUEDT_READING = 0;
+	
 	//创建界面
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 		setContentView(new ConchSurface(this));
+		
+		//动态权限
+		if(Build.VERSION.SDK_INT>=23)
+		{
+			int checkRead=ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE);
+			if(checkRead!=PackageManager.PERMISSION_GRANTED)
+			{
+				ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEDT_READING);
+			}
+		}
     }
 
 	//设置退出按钮
@@ -30,5 +48,26 @@ public class Shower extends AppCompatActivity
 			return true;
 		}
 		return super.onKeyDown(keyCode,event);
+	}
+
+	//许可请求回调
+	@Override
+	public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResults)
+	{
+		// TODO: Implement this method
+		if(requestCode==REQUEDT_READING)
+		{
+			if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+			{
+				return;
+			}
+			else
+			{
+				Toast.makeText(this,"Can't read sd card,finish running.",Toast.LENGTH_SHORT).show();
+				finish();
+				return;
+			}
+		}
+		super.onRequestPermissionsResult(requestCode,permissions,grantResults);
 	}
 }
--- GraphisTool/app/src/main/java/org/tonygatins/tonysmith/conch/resource/ConchFont.java
+++ GraphisTool/app/src/main/java/org/tonygatins/tonysmith/conch/resource/ConchFont.java
@@ -1,25 +1,30 @@
 package org.tonygatins.tonysmith.conch.resource;
 
 import android.graphics.Typeface;
-import java.util.HashMap;
-import java.util.Map;
 import android.os.Build;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 import org.xml.sax.Attributes;
-import org.xml.sax.Locator;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * 资源：字体。
  * 由于实现问题，这里不会解耦本地代码，搬运至其他平台时需要重写。
- * 软加载字体，不需要时只保留路径。
+ * 软加载字体，不需要时只保留路径。需要回收
  */
 public final class ConchFont
 {
 	//字体资源对象
-	private Typeface resource;
+	private final Typeface resource;
 	
 	//字体家族
 	private static final Map<String,ConchFont> FONTS_FAMILY=new HashMap<>();
@@ -27,8 +32,12 @@ public final class ConchFont
 	//字体文件路径
 	private static final Map<String,String> FONTS_PATHS=new HashMap<>();
 	
-	//是否为系统自带
-	private boolean isSystemInclude;
+	//构造一个字体对象。
+	private ConchFont(String key)
+	{
+		resource=Typeface.createFromFile(FONTS_PATHS.get(key));
+		FONTS_FAMILY.put(key,this);
+	}
 	
 	//下面耦合性很高
 	
@@ -79,8 +88,13 @@ public final class ConchFont
 		{
 			XMLReader reader19 = XMLReaderFactory.createXMLReader();
 			reader19.setContentHandler(new FontsConfig19());
+			reader19.parse(new InputSource(configPath));
 		}
 		catch(SAXException e)
+		{
+			throw new RuntimeException(e);
+		}
+		catch(IOException e)
 		{
 			throw new RuntimeException(e);
 		}
@@ -89,6 +103,130 @@ public final class ConchFont
 	//解析接口 19实现
 	private static class FontsConfig19 extends DefaultHandler
 	{
-		protected FontsConfig19(){}
+		//元素内容
+		private List<String> alias = new LinkedList<>();
+		private List<String> fileName = new LinkedList<>();
+		private List<String> fixedAlias = new LinkedList<>();
+		private List<String> fixedKey = new LinkedList<>();
+		private String variant="";
+		
+		//是否到字体家族，名字集，文件集。
+		private boolean isStartFontFamily=false,isStartNameSet=false,isStartFileSet=false;
+		
+		protected FontsConfig19()
+		{}
+
+		//开始分析元素
+		@Override
+		public void startElement(String uri,String localName,String qName,Attributes attributes)
+		{
+			// TODO: Implement this method
+			if(localName.equals("family"))
+			{
+				isStartFontFamily=true;
+			}
+			
+			if(localName.equals("name")&&isStartFontFamily)
+			{
+				isStartNameSet=true;
+			}
+			
+			if(localName.equals("file")&&isStartFontFamily)
+			{
+				isStartFileSet=true;
+				if(attributes.getLength()!=0)
+				{
+					variant=attributes.getValue(0).concat("_");
+				}
+			}
+		}
+		
+		//截取字符串
+		@Override
+		public void characters(char[] ch,int start,int length)
+		{
+			// TODO: Implement this method
+			if(isStartNameSet)
+			{
+				alias.add(new String(ch,start,length));
+			}
+			
+			if(isStartFileSet)
+			{
+				fileName.add(variant.concat(new String(ch,start,length)));
+				variant="";
+			}
+		}
+
+		//结束分析元素
+		@Override
+		public void endElement(String uri,String localName,String qName)
+		{
+			// TODO: Implement this method
+			if(localName.equals("family"))
+			{
+				isStartFontFamily=false;
+				
+				//别名处理
+				if(alias.size()!=0)
+				{
+					for(String simple : alias)
+					{
+						int index=simple.indexOf("sans-serif");
+						if(index>=0)
+						{
+							fixedAlias.add("SansSerif");
+							continue;
+						}
+						
+						int index1 = 0;
+						boolean isStart = false;
+						StringBuilder newAilas = new StringBuilder(simple.length());
+						char temp[]=simple.toCharArray();
+						for(int tempItr=0;tempItr<temp.length;tempItr++)
+						{
+							if(Character.isLetter(temp[tempItr]))
+							{
+								if(!isStart)
+								{
+									isStart=!isStart;
+									index1=tempItr;
+								}
+								
+								if(tempItr==temp.length-1)
+								{
+									if(Character.isLowerCase(temp[index1]))
+									{
+										temp[index1]=(char)(temp[index1]-32);
+									}
+									newAilas.append(new String(temp,index1,tempItr-index1+1));
+									isStart=!isStart;
+								}
+							}
+							
+							if(Character.isSpace(temp[tempItr])&&isStart)
+							{
+								if(Character.isLowerCase(temp[index1]))
+								{
+									temp[index1]=(char)(temp[index1]-32);
+								}
+								newAilas.append(new String(temp,index1,tempItr-index1));
+								isStart=!isStart;
+							}
+						}
+					}
+				}
+			}
+
+			if(localName.equals("name")&&isStartFontFamily)
+			{
+				isStartNameSet=false;
+			}
+
+			if(localName.equals("file")&&isStartFontFamily)
+			{
+				isStartFileSet=false;
+			}
+		}
 	}
 }
